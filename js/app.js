80356b81b13b4888b2ddfbc0a247107f28a541a5
brendan <brendan.heberton@gmail.com>
2013-04-25 16:02:32 -0600

    map working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  ready: function() {
+    this.set('usaStatesGeoData', usaStatesGeoData);
+  }
+});
 
 App.Router.map(function() {
   // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+  //nothing
+});
+
+
+App.Map = Ember.View.extend({
+  path: function() {
+    var mapW  = this.$().height();
+    var mapH  = this.$().width();
+    var mapXY = d3.geo.albersUsa().scale(700).translate([mapH/2,mapW/2]);
+
+    return d3.geo.path().projection(mapXY);
+  }.property(),
+  
+  didInsertElement: function() {
+    var elementId = this.get('elementId');
+    var regions = d3.select("#" + elementId).append("svg").append("g").attr("id", "regions");
+    var features = this.get('geoData').features;
+    var path = this.get('path');
+
+    regions.selectAll("#regions path").data(features).enter().insert("path")
+      .attr("d",      path)
+      .attr('stroke', '#ccc'  )
+      .attr('fill',   'white' );
   }
 });
