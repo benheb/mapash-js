60b2c4786df2602893da01cb31f063718c870d9c
brendan <brendan.heberton@gmail.com>
2013-05-01 14:43:05 -0600

    searching agol

--- js/app/views/finder.js
+++ js/app/views/finder.js
@@ -14,8 +14,32 @@ define('app/views/finder', [
 			elementId: 'find',
 			template: Ember.Handlebars.compile( html ),
       didInsertElement: function(){
+        
+        //Test Data
         $('#' + this.elementId + ' #add').on('click', function(){
           Map.mapController.addLayer({title: 'Colorado Snow Totals', url: 'data/snow.json'});
+        });
+        
+        //search AGOL
+        $('#' + this.elementId + ' #search-agol').on('keyup', function(){
+          var val = $(this).val();
+          
+          Map.arcgis.search( { q: val }, function( err, res ){
+            if ( res ) {
+              $('#search-items').empty();
+              $.each(res.results, function(i,f){
+                var item = '<li class="search-item-result">'+ f.title +'</li>';
+                $('#search-items').append(item)
+              });
+              
+            }
+            if ( err ) { $('#search-items').empty(); console.log( 'error: ', err) };
+          });
+        });
+        
+        //Add Selected Search Item
+        $('#' + this.elementId + ' .search-item-result').on('click', function(){
+          console.log('Add: ', $(this).html());
         });
       }
     });
