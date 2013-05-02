8e4a6e46215667a3f0d0c0d77af92d3e543c5044
chelm <christopher_helm@esri.com>
2013-05-02 10:14:31 -0600

    messed up

--- js/app/views/application.js
+++ js/app/views/application.js
@@ -1,33 +1,33 @@
 define('app/views/application', [
-		'app/views/sidebar',
-		'app/views/layers',
-		'app/views/map',
-		'text!app/templates/header.html',
-		'ember'
-	],
-	/**
-	 * Main application view
-	 *
-	 * @param Class StatsView, stats view class
-	 * @returns Class
-	 */
-	function( SidebarView, LayersView, MapView, header_html ) {
-		return Ember.ContainerView.extend({
-				elementId: 'app_container',
-				childViews: [ 'headerView', 'mainView' ],
-				headerView: Ember.ContainerView.create({
-					childViews: [ 'titleView'],
-					elementId: 'header',
-					tagName: 'header',
-					titleView: Ember.View.create({
+    'app/views/sidebar',
+    'app/views/layers',
+    'app/views/map',
+    'text!app/templates/header.html',
+    'ember'
+  ],
+  /**
+   * Main application view
+   *
+   * @param Class StatsView, stats view class
+   * @returns Class
+   */
+  function( SidebarView, LayersView, MapView, header_html ) {
+    return Ember.ContainerView.extend({
+        elementId: 'app_container',
+        childViews: [ 'headerView', 'mainView' ],
+        headerView: Ember.ContainerView.create({
+          childViews: [ 'titleView'],
+          elementId: 'header',
+          tagName: 'header',
+          titleView: Ember.View.create({
             template: Ember.Handlebars.compile( header_html )
-					})
-				}),
-				mainView: Ember.ContainerView.create({
+          })
+        }),
+        mainView: Ember.ContainerView.create({
           template: Ember.Handlebars.compile("<div class='row'>{{yield}}</div>"),
-					elementId: 'map_main',
-					classNames: [''],
-				  childViews: [ 
+          elementId: 'map_main',
+          classNames: [''],
+          childViews: [ 
             SidebarView.create(), 
             LayersView.create(), 
             Ember.ContainerView.create({
@@ -35,7 +35,7 @@ define('app/views/application', [
               childViews: [ MapView.create() ]
             })
           ]
-				})
-			})
-	}
+        })
+      })
+  }
 );
